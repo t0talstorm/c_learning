e2146cccc15864d2f07630a3da87f5bdfc538977
t0talstorm <adityaishan007@gmail.com>
2024-09-19 22:07:45 +0530

    For loop

---
+++ Loops/for_loop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    
+    for(int i = 1 ; i <=10;i++){// Entry control loop
+    // can use i+ = x to increment by 'x' where x is any int 
+    // can use i- = x to decrement by 'x' where x is any int 
+
+    printf("%d\n",i);
+
+    }
+}
