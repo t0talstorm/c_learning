ce313afcb3013e1c7d03e359cbe0111e32d98bfe
t0talstorm <adityaishan007@gmail.com>
2024-09-20 20:35:49 +0530

    do while loop

---
+++ Loops/do_while_loop.c
@@ -0,0 +1,16 @@
+//  do while loop atleast executes the loop atleast once 
+
+#include <stdio.h>
+
+int main() {
+    int i = 0;
+
+    do {
+        printf("%d\n", i);
+        i++;
+
+
+    } while (i <= 5); // Exit control loop
+
+    return 0;
+}
