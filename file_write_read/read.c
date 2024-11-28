d25270cba96e9a569973fec6445e63b0fc817b48
Aditya Sinha <sinha.aditya.btech2024@sitpune.edu.in>
2024-11-28 08:58:28 +0000

    class programs

--- file_write_read/read.c
+++ file_write_read/read.c
@@ -2,19 +2,29 @@
 
 int main(){
 
-    FILE *pF = fopen("D:\\poem.txt" , "r");   
-    char buffer[255];
+    FILE *pF = fopen("poem.txt" , "r"); 
+    /* the modes in which file pointer can be opened are 
+      r, w, a, r+, w+, a+, rb, wb, ab, rb+, wb+, ab+ 
+    which stand for
+     read, write, append, read and write, write and read, append and read, read in binary, write in binary, append in binary, read and write in binary, write and read in binary, append and read in binary respectively.   
+    */
+   
+   char buffer[255];
 
- if(pF == NULL){
+    if(pF == NULL){
 
-    printf("unable to open file");
- }
-else{
-    while (fgets(buffer, 255 ,pF) != NULL) {
-    
-        printf("%s",buffer);
+        printf("unable to open file");
     }
-}
+    else{
+        while (fgets(buffer, 255 ,pF) != NULL) {
+        
+            printf("%s",buffer);
+        }
+    }
+
+    printf("\n");
     fclose(pF);
+
     return 0;
+
 }
---
+++ misc/memory_aloc_2.c
@@ -0,0 +1,2 @@
+//malloc accepts only one argument, the size of the memory block in bytes.
+// for realoc to work the pointer must be created using malloc 
